dab4604936c21764cb67f5784c6d90a0de4bbcbb
Josué Quesada <josuequesadaxd@gmail.com>
2024-10-22 22:09:30 -0600

    update

--- admin-helper/src/pages/CashClosing/CashClosing.tsx
+++ admin-helper/src/pages/CashClosing/CashClosing.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export const CashClosing = () => {
   return (
-    <div>CashClosing</div>
-  )
-}
-
+    <div>
+      <h2>Cierre de Caja</h2>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Active
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">
+            Disabled
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
