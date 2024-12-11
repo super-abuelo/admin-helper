771f38a493b643b05aecf001e98f506f0516177d
Josué Quesada <josuequesadaxd@gmail.com>
2024-12-11 16:38:06 -0600

    updated bills table

--- admin-helper/src/components/bills/BillsForm.tsx
+++ admin-helper/src/components/bills/BillsForm.tsx
@@ -3,12 +3,60 @@ import React from "react";
 function BillsForm() {
   return (
     <div>
-      BillsForm
-      <h1>knede</h1>
-      <h1>knede</h1>
-      <h1>knede</h1>
-      <h1>knede</h1>
-      <h1>knede</h1>
+      <h1 className="my-3">Billetes</h1>
+      <div className="d-flex justify-content-center text-center">
+        <table className="table table-striped-columns table-bordered w-50 shadow">
+          <thead>
+            <tr>
+              <th className="col-4">Denominación</th>
+              <th className="col-4">Cantidad</th>
+              <th className="col">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <label className="my-2">20 000</label>
+              </td>
+              <td>
+                <input className="form-control border-0 text-center" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="my-2">10 000</label>
+              </td>
+              <td>
+                <input className="form-control border-0 text-center" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="my-2">5 000</label>
+              </td>
+              <td>
+                <input className="form-control border-0 text-center" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="my-2">2 000</label>
+              </td>
+              <td>
+                <input className="form-control border-0 text-center" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="my-2">1 000</label>
+              </td>
+              <td>
+                <input className="form-control border-0 text-center" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- admin-helper/src/components/credits/CreditsForm.tsx
+++ admin-helper/src/components/credits/CreditsForm.tsx
@@ -3,22 +3,22 @@ import React from "react";
 function CreditsForm() {
   return (
     <div>
-      <h1 className="my-2">Créditos</h1>
+      <h1 className="my-3">Créditos</h1>
       <div className="d-flex justify-content-center">
         <table className="table table-striped-columns table-bordered w-50 shadow">
           <thead>
             <tr>
-              <th>Créditos - Nombres</th>
-              <th>Monto</th>
+              <th className="col-8">Créditos - Nombres</th>
+              <th className="col">Monto</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>
-                <input className="form-control border-0"/>
+                <input className="form-control border-0" />
               </td>
               <td>
-                <input className="form-control border-0 w-50" />
+                <input className="form-control border-0 text-center" />
               </td>
             </tr>{" "}
             <tr>
@@ -26,7 +26,7 @@ function CreditsForm() {
                 <input className="form-control border-0" />
               </td>
               <td>
-                <input className="form-control border-0 w-50" />
+                <input className="form-control border-0 text-center" />
               </td>
             </tr>{" "}
             <tr>
@@ -34,7 +34,7 @@ function CreditsForm() {
                 <input className="form-control border-0" />
               </td>
               <td>
-                <input className="form-control border-0 w-50" />
+                <input className="form-control border-0 text-center" />
               </td>
             </tr>
           </tbody>
