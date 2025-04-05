ff1c9ffdd004f693c414478d39f933ef02f09500
Josué Quesada <josuequesadaxd@gmail.com>
2025-04-05 11:46:18 -0600

    updated page positions

--- src/pages/RegisterClosings/RegisterClosings.tsx
+++ src/pages/RegisterClosings/RegisterClosings.tsx
@@ -97,9 +97,7 @@ export const RegisterClosings = () => {
               ) : currentItems.length > 0 ? (
                 currentItems.map((cierre: any) => (
                   <tr key={cierre.id}>
-                    <td>
-                      {cierre.fecha}
-                    </td>
+                    <td>{cierre.fecha}</td>
                     <td>{cierre.superMercado}</td>
                     <td>{cierre.caja ?? "N/A"}</td>
                     <td>{cierre.usuario?.toString() || "N/A"}</td>
@@ -128,65 +126,71 @@ export const RegisterClosings = () => {
           </table>
         </div>
       )}
-      <div className="d-flex justify-content-center align-items-center">
-        <nav aria-label="Page navigation">
-          <ul className="pagination">
-            <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
-              <button
-                className="page-link"
-                onClick={() => handlePageChange(currentPage - 1)}
-                disabled={currentPage === 1}
+      {!showForm && (
+        <div className="d-flex justify-content-center align-items-center">
+          <nav aria-label="Page navigation">
+            <ul className="pagination">
+              <li
+                className={`page-item ${currentPage === 1 ? "disabled" : ""}`}
               >
-                Anterior
-              </button>
-            </li>
-
-            {[...Array(Math.ceil(data.length / itemsPerPage))].map(
-              (_, index) => (
-                <li
-                  key={index + 1}
-                  className={`page-item ${
-                    index + 1 === currentPage ? "active" : ""
-                  }`}
+                <button
+                  className="page-link"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                  disabled={currentPage === 1}
                 >
-                  <button
-                    className="page-link"
-                    onClick={() => handlePageChange(index + 1)}
-                  >
-                    {index + 1}
-                  </button>
-                </li>
-              )
-            )}
+                  Anterior
+                </button>
+              </li>
 
-            <li
-              className={`page-item ${
-                currentPage === Math.ceil(data.length / itemsPerPage)
-                  ? "disabled"
-                  : ""
-              }`}
-            >
-              <button
-                className="page-link"
-                onClick={() => handlePageChange(currentPage + 1)}
-                disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
+              {[...Array(Math.ceil(data.length / itemsPerPage))].map(
+                (_, index) => (
+                  <li
+                    key={index + 1}
+                    className={`page-item ${
+                      index + 1 === currentPage ? "active" : ""
+                    }`}
+                  >
+                    <button
+                      className="page-link"
+                      onClick={() => handlePageChange(index + 1)}
+                    >
+                      {index + 1}
+                    </button>
+                  </li>
+                )
+              )}
+
+              <li
+                className={`page-item ${
+                  currentPage === Math.ceil(data.length / itemsPerPage)
+                    ? "disabled"
+                    : ""
+                }`}
               >
-                Siguiente
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </div>
+                <button
+                  className="page-link"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  disabled={
+                    currentPage === Math.ceil(data.length / itemsPerPage)
+                  }
+                >
+                  Siguiente
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
       {showForm && (
-        <div>
+        <div className="reviewContainer">
           <ReviewForm closingData={selectedData} cierreId={selectedData.id} />
           <div className="my-3">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary mb-5">
               Guardar
             </button>
             <button
               type="button"
-              className="btn btn-secondary ms-3"
+              className="btn btn-secondary ms-3 mb-5"
               onClick={handleBack}
             >
               Volver
