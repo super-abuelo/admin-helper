b72e0d588c1183a116ba8cd5196e9bdffdad6d3d
Josué Quesada <josuequesadaxd@gmail.com>
2025-04-07 12:42:42 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/admin-helper",
+  base: "/admin-helper/",
   plugins: [react()],
 })
 
